9235a2200359ea434fc5efb285dca0ed3c613519
Olooce <121952433+Olooce@users.noreply.github.com>
2025-03-17 17:00:03 +0300

    chore: Update model/model.go
    modified: model/model.go

--- internal/model/model.go
+++ internal/model/model.go
@@ -9,7 +9,7 @@ type User struct {
 	Password                   string    `json:"password,omitempty"` // Exclude from JSON responses
 	FirstName                  string    `json:"first_name"`
 	LastName                   string    `json:"last_name"`
-	InitialAssessmentCompleted bool      `json:"initial_assessment_completed" gorm:"-"`
+	InitialAssessmentCompleted bool      `json:"initial_assessment_completed" gorm:"default:false"`
 	CreatedAt                  time.Time `json:"created_at"`
 	UpdatedAt                  time.Time `json:"updated_at"`
 }
